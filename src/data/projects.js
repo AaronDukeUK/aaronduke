458c155af49f713db28a42da4201d9e14105a96d
Aaron Duke <hello@aaronduke.co.uk>
2023-05-28 20:30:26 +0100

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+import dwa from "../assets/images/dwa.svg"
+import clock from "../assets/images/clock.jpg"
+import audio from "../assets/images/audio.png"
+import ono from "../assets/images/ono.png"
+
+export const projects = [
+  {
+    title: "Audiophile",
+    link: "https://adaudiophile.netlify.app/",
+    github: "https://github.com/AaronDukeUK/ad-audiophile",
+    image: audio,
+    text: "Audiophile is a website specialising in high-end audio equipment.",
+    completed: true,
+    tags: ["#React", "#Stripe", "#Sanity"],
+  },
+  {
+    title: "Onoglams",
+    link: "https://onoglams.myshopify.com",
+    image: ono,
+    text: "Onoglams is a custom Shopify Website created with Shopify Liquid",
+    completed: true,
+    tags: ["#Shopify", "#Liquid", "#Sass"],
+  },
+  {
+    title: "Clock App",
+    link: "https://ad-clock-app.netlify.app/",
+    github: "https://github.com/AaronDukeUK/clockapp",
+    image: clock,
+    text: "A clock app displaying the current time, date and day of the week.",
+    completed: true,
+    tags: ["#React", "#Sass"],
+  },
+  {
+    title: "Dick White Academy",
+    link: "https://dickwhiteacademy.netlify.app/",
+    github: "https://github.com/AaronDukeUK/DickWhiteAcademy",
+    image: dwa,
+    text: "Dick White Academy is a website for a local veterinary nurse college.",
+    completed: true,
+    tags: ["#Astro", "#Sanity", "#Sass"],
+  },
+]
---
+++ src/data/strips.js
@@ -0,0 +1,11 @@
+export const strip = {
+  text: "I am passionate about personal development, always keeping up-to-date with the latest technologies. Now looking for new opportunities to work with forward-thinking companies and individuals.",
+  btnText: "VIEW CV",
+  btnLink: "/",
+}
+
+export const strip2 = {
+  text: "Front-End Developer with a background in Hospitality Management, bringing a unique perspective to the industry. Away from coding, I have a passion for cooking, walking the dogs and Euro games.",
+  btnText: "LINKEDIN",
+  btnLink: "https://www.linkedin.com/in/aduke-dev/",
+}
